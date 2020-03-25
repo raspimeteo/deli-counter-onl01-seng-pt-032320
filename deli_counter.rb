b968e68626cdf58870a5707c8f3ffaf4d6ddcb14
John Zonneveld <jjczon@gmail.com>
2020-03-25 20:07:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+    if katz_deli.size == 0
+        puts "The line is currently empty."
+    else
+        counter = 1
+        print 'The line is currently: '
+        katz_deli.each do |in_line|
+            puts "#{counter}. #{in_line} "
+            counter += 1
+        end
+    end
+end
